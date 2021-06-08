c8582ab29936bde2443993d705988693fc98feba
ruh2212 <83959660+ruh2212@users.noreply.github.com>
2021-06-08 18:35:38 +0530

    Create flip array.php

---
+++ Day5/flip array.php
@@ -0,0 +1,13 @@
+<?php
+$a = array(
+	'Name' => "Ruchi Chiragkumar Patel",
+	'Usr_Name' => "ruh-2212",
+	'Mobile' => 7575082212,
+	'Address' => "A6 klyan nagae socitety ",
+	'Task' => "flip details",
+);
+echo "<pre>";
+print_r($a);
+echo "<pre> Flipped ";
+print_r(array_flip($a));
+?>
