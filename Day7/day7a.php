1df8b922dac7f9729488a9a1a743d2ecc57fe83b
ruh2212 <83959660+ruh2212@users.noreply.github.com>
2021-06-14 09:42:25 +0530

    Create day7a.php

---
+++ Day7/day7a.php
@@ -0,0 +1,44 @@
+<?php
+
+$connection = mysqli_connect("localhost","root","","db_intrenship");
+ 
+//query
+$q = mysqli_query($connection,"select * from tbl_user") or die(mysqli_error($connection));
+
+echo"<table border='1'>";
+
+echo"<tr>";
+echo"<th>ID</th>";
+echo"<th>NAME</th>";
+echo"<th>GENDER</th>";
+echo"<th>DOB</th>";
+echo"<th>Email</th>";
+echo"<th>Mobile No.</th>";
+echo"<th>Address</th>";
+echo"<th>Password</th>";
+echo"<th>Area</th>";
+echo"<th>Pincode</th>";
+echo"<th>Bloodgroup</th>";
+echo"<th>action</th>";
+echo"</tr>";
+while ($row = mysqli_fetch_array($q))
+{
+    echo"<tr>";
+    echo "<td>{$row['st_id']}</td>";
+    echo "<td>{$row['st_name']}</td>";
+    echo "<td>{$row['st_gender']}</td>";
+    echo "<td>{$row['st_dob']}</td>";
+    echo "<td>{$row['st_email']}</td>";
+    echo "<td>{$row['st_mobile']}</td>";
+    echo "<td>{$row['st_address']}</td>";
+    echo "<td>{$row['st_password']}</td>";
+    echo "<td>{$row['st_area']}</td>";
+    echo "<td>{$row['st_pincode']}</td>";
+    echo "<td>{$row['st_bloodgroup']}</td>";
+    echo "<td><a href='delete.php?deleteid={$row['st_id']}'>deletet</a></td>";
+    echo"</tr>";
+}
+
+echo"</table>";
+
+echo "<a href ='insert.php'>add record</a>";
